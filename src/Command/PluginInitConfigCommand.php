40e20a2b687abd440d37e955a9687774f5531bf3
bogdanovandreycode <bogdanov.andrey.code@gmail.com>
2025-11-07 17:24:20 +0200

    Add force option to PluginInitConfigCommand for overwriting existing config files

--- src/Command/PluginInitConfigCommand.php
+++ src/Command/PluginInitConfigCommand.php
@@ -4,6 +4,7 @@ namespace Wpkit\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class PluginInitConfigCommand extends Command
@@ -12,21 +13,28 @@ class PluginInitConfigCommand extends Command
     {
         $this
             ->setName('plugin:init-config')
-            ->setDescription('Copies sample plugin-config.json into the current directory.');
+            ->setDescription('Copies sample plugin-config.json into the current directory.')
+            ->addOption(
+                'force',
+                'f',
+                InputOption::VALUE_NONE,
+                'Overwrite existing config file if it already exists.'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $source = __DIR__ . '/../../plugin-config-sample.json';
         $destination = getcwd() . '/plugin-config.json';
+        $force = $input->getOption('force');
 
         if (!file_exists($source)) {
             $output->writeln("<error>Sample config file not found: {$source}</error>");
             return Command::FAILURE;
         }
 
-        if (file_exists($destination)) {
-            $output->writeln("<comment>plugin-config.json already exists in this directory.</comment>");
+        if (file_exists($destination) && !$force) {
+            $output->writeln("<comment>plugin-config.json already exists. Use --force to overwrite.</comment>");
             return Command::FAILURE;
         }
 
